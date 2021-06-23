317832946ac5618fe921faa46eafd1e347254b8f
Haotian Weng <haotian.weng@hotmail.com>
2021-06-24 00:56:20 +1000

    Update control panel

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
-import argparse
-import subprocess
+"""
+@author Haotian Weng
+@email haotian.weng@anu.edu.au
+@create date 2021-06-24
+@modify date 2021-06-24
+@desc Parse arguments and run functions
+"""
 
-from src import url_generator
+import argparse
+from src import url_generator, url_scraper, scanner
 
 
 def main():
     parser = argparse.ArgumentParser(description="Tor Scanner - Scan the Onion hidden services")
-    parser.add_argument("-g", "--generate", help="Sequentially generate unique Onion URLs")
-    parser.add_argument("-S", "--scrape", help="Scrape Onion URLs from ahmia search engine")
-    parser.add_argument("-p", "--ping", help="Ping Onion URLs and write active URLs to a file")
-    parser.add_argument("-s", "--scan", help="Scan ports of Onion URLs and write results to a file")
+    parser.add_argument("-g", "--generate", type=int, choices=[2, 3], help="Sequentially generate unique v2 or v3 Onion URLs. Select 2 or 3 to generate v2 or v3 Onion URLs, respectively.")
+    parser.add_argument("-S", "--scrape", action="store_true", help="Scrape Onion URLs from ahmia search engine.")
+    parser.add_argument("-p", "--ping", action="store_true", help="Ping Onion URLs and write active URLs to a file.")
+    parser.add_argument("-s", "--scan", action="store_true", help="Scan ports of Onion URLs and write results to a file.")
     args = parser.parse_args()
 
     if args.generate:
-        url_generator.generate_urls()
+        if args.generate == 2:
+            url_generator.generate_urls(2, "data/onion_urls.txt")
+        elif args.generate == 3:
+            url_generator.generate_urls(3, "data/onion_urls.txt")
+        else:
+            raise ValueError("Invalid version number!")
+    elif args.scrape:
+        url_scraper.scrape_urls("data/onion_urls.txt")
+    elif args.ping:
+        scanner.ping_onions("data/onion_urls.txt", "data/active_onion_urls.txt")
+    elif args.scan:
+        scanner.scan_ports("data/active_onion_urls.txt", "data/onions_ports.csv")
     else:
-        print("Please supply an argument!")
+        raise Exception("Please supply an argument!")
 
 if __name__ == "__main__":
     main()
